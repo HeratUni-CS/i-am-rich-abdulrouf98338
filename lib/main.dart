6b41e6391b66952272a245e8db0473a76459918a
abdulrouf <roufraghozar1400@gmail.com>
2023-03-30 11:34:00 -0700

    background changed

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,17 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(
-            child: Text('Hellow world'),
+          backgroundColor: Colors.black12,
+          appBar: AppBar(
+            title: Center(
+              child: Text('Hello world'),
+            ),
           ),
-        ),
-        body: Center(child:Image(image:AssetImage('images/new.png'),),)
-      ),
+          body: Center(
+            child: Image(
+              image: AssetImage('images/new.png'),
+            ),
+          )),
     ),
   );
 }
